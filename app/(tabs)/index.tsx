8aac2755e887cf22e2c4040630fc637e69e52e46
jhonatan3307 <jhonatanlisboaaj@gmail.com>
2025-05-28 15:34:35 +0000

    Mudança_index

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,75 +1,88 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { Text, TouchableOpacity, View, StyleSheet } from 'react-native';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-
-export default function HomeScreen() {
+export default function Index() {
+  const cell = () => {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Bem vindo!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+      <TouchableOpacity style={styles.square}>
+      <text style={styles.symbol}/>X</text>
+      </TouchableOpacity>
   );
+  };
+  return (
+    <><view style={styles.container}>
+      <text style={styles.title} Jogo De Velhas /></text>
+      <text style={styles.status} Vencedor text>
+
+        <view style={styles.board}>
+        <view style={styles.row}>
+            <TouchableOpacity>
+              <text>
+                X
+              </text>
+            </TouchableOpacity>
+          </view>
+        </view>
+        <view style={styles.row}>
+          <view>
+            <TouchableOpacity>
+              <text>
+                X
+              </text>
+            </TouchableOpacity>
+          </view>
+        </view>
+        <view style={styles.row}>
+          <view>
+            <TouchableOpacity>
+              <text>
+                X
+              </text>
+            </TouchableOpacity>
+          </view>
+        </view>
+        <TouchableOpacity style={styles.ResetButton}>
+        <Text><RESETAR></RESETAR></Text>
+        </TouchableOpacity>
+      </view></>
+  )
+
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
+  container: {
+    flex: 1,
     alignItems: 'center',
-    gap: 8,
+    justifyContent: 'center',
+    backgroundColor: 'blue'
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  status: {
+    fontSize: 18,
+    marginBottom: 20,
   },
-});
+  row: {
+    flexDirection: 'row'
+  },
+  square: {
+    width: 80,
+    height: 80,
+    borderWidth: 2,
+    borderColor: "#333333",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#FFFFFF",
+  },
+  symbol: {
+    fontSize: 36,
+    fontWeight: "bold",
+  },
+  ResetButton: {
+    padding:10,
+    backgroundColor: '#FFFFFF'
+    fontSize:16,
+  }
+})
